71bb407909de793b3f031babc01f8ebe74899759
onlyet <169212327@qq.com>
2017-10-02 16:18:46 +0800

    Firstly commit source code

---
+++ low_open.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+void error_handling(char* message);
+
+int main(void)
+{
+	int fd;
+	char buf[] = "Let's go!\n";
+
+	fd=open("data.txt", O_CREAT|O_RDWR|O_TRUNC);
+	if(fd==-1)
+		error_handling("open() error!");
+	printf("file decriptor: %d \n", fd);
+
+	if(write(fd, buf, sizeof(buf))==-1)
+		error_handling("write() error!");
+	close(fd);
+	return 0;
+}
+
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+	
