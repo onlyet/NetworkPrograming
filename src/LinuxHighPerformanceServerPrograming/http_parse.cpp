ab3efc34af1ae98a285ebae9c92cbb387017bb6f
onlyet <1369212327@qq.com>
2018-07-13 00:26:05 +0800

    1

--- src/LinuxHighPerformanceServerPrograming/http_parse.cpp
+++ src/LinuxHighPerformanceServerPrograming/http_parse.cpp
@@ -1,4 +1,4 @@
-#include <sys/socket.h>
+﻿#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <assert.h>
@@ -27,7 +27,7 @@ enum HTTP_CODE {
 	GET_REQUEST,
 	BAD_REQUEST,
 	FORBIDDEN_REQUEST,
-	INTERNAL_REQUEST,
+	INTERNAL_ERROR,
 	CLOSED_REQUEST
 };
 
@@ -129,13 +129,99 @@ HTTP_CODE parse_content(char* buffer, int& checked_index,
 		{
 			retcode = parse_requestline(temp, checkstate);
 			if (retcode == BAD_REQUEST) {
-				
+				return BAD_REQUEST;
 			}
+			break;
+		}
+		case CHECK_STATE_HEADER:
+		{
+			retcode = parse_headers(temp);
+			if (retcode == BAD_REQUEST) {
+				return BAD_REQUEST;
+			}
+			else if (retcode == GET_REQUEST) {
+				return GET_REQUEST;
+			}
+			break;
+		}
+		default:
+		{
+			return INTERNAL_ERROR;
 		}
 		}
 	}
+	if (linestatus == LINE_OPEN) {
+		return NO_REQUEST;
+	}
+	else {
+		return BAD_REQUEST;
+	}
 }
 
+int main(int argc, char *argv[])
+{
+	if (argc <= 2) {
+		printf("usage: %s ip_address port_number\n", argv[0]);
+		return 1;
+	}
+	const char *ip = argv[1];
+	int port = atoi(argv[2]);
+
+	struct sockaddr_in address;
+	bzero(&address, sizeof(address));
+	address.sin_family = AF_INET;
+	inet_pton(AF_INET, ip, &address.sin_addr);
+	address.sin_port = htons(port);
+
+	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	assert(listenfd >= 0);
+	int ret = bind(listenfd, (struct sockaddr*)&address, sizeof(address));
+	assert(ret != -1);
+	ret = listen(listenfd, 5);
+	assert(ret != -1);
+	struct sockaddr_in client_address;
+	socklen_t client_addrlength = sizeof(client_address);
+	int fd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlength);
+	if (fd < 0) {
+		printf("errno is: %d\n", errno);
+	}
+	else {
+		char buffer[BUFFER_SIZE];
+		memset(buffer, '\0', BUFFER_SIZE);
+		int data_read = 0;
+		int read_index = 0;
+		int checked_index = 0;
+		int start_line = 0;
+		CHECK_STATE checkstate = CHECK_STATE_REQUESTLINE;
+		while (1) {
+			data_read = recv(fd, buffer + read_index, BUFFER_SIZE - read_index, 0);
+			if (data_read == -1) {
+				printf("reading failed\n");
+				break;
+			}
+			else if (data_read == 0) {
+				printf("remote client has closed the connection\n");
+				break;
+			}
+			read_index += data_read;
+			HTTP_CODE result = parse_content(buffer, checked_index, checkstate, read_index, start_line);
+			if (result == NO_REQUEST) {
+				continue;
+			}
+			else if (result == GET_REQUEST) {
+				send(fd, szret[0], strlen(szret[0]), 0);
+				break;
+			}
+			else {
+				send(fd, szret[1], strlen(szret[1]), 0);
+				break;
+			}
+		}
+		close(fd);
+	}
+	close(listenfd);
+	return 0;
+}
 
 
 
