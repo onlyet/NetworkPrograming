e755210fdfbe0f781d875a7ad1285acbb5122c0e
onlyet <1369212327@qq.com>
2018-12-27 17:52:08 +0800

    1

--- Windows/AsynNotiEcho.cpp
+++ Windows/AsynNotiEcho.cpp
@@ -1,140 +1,140 @@
-﻿#include <WinSock2.h>
-#include <stdio.h>
-#include <string.h>
-//#include <winsock2.h>
-
-#define BUF_SIZE 100
-
-void CompressSockets(SOCKET hSockArr[], int idx, int total);
-void CompressEvents(WSAEVENT hEventArr[], int idx, int total);
-void ErrorHandling(char* msg);
-
-int main(int argc, char* argv[])
-{
-	WSADATA wsaData = { 0 };
-	SOCKET hServSock, hClntSock;
-	SOCKADDR_IN servAdr, clntAdr;
-
-	SOCKET hSockArr[WSA_MAXIMUM_WAIT_EVENTS] = { 0 };
-	WSAEVENT hEventArr[WSA_MAXIMUM_WAIT_EVENTS] = { 0 };
-	WSAEVENT newEvent;
-	WSANETWORKEVENTS netEvents;
-
-	int numofClntSock = 0;
-	int strLen, i;
-	int posInfo, startIdx;
-	int clntAdrLen;
-	char msg[BUF_SIZE] = { 0 };
-
-	if (argc != 2) {
-		printf("Usage: %s <port>\n", argv[0]);
-		exit(1);
-	}
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		ErrorHandling("WSAStartup error");
-
-	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAdr.sin_port = htons(atoi(argv[1]));
-
-	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
-		ErrorHandling("bind error");
-
-	if (listen(hServSock, 5) == SOCKET_ERROR)
-		ErrorHandling("listen error");
-
-	newEvent = WSACreateEvent();
-	if (WSAEventSelect(hServSock, newEvent, FD_ACCEPT) == SOCKET_ERROR)
-		ErrorHandling("WSAEventSelect erorr");
-	
-	hSockArr[numofClntSock] = hServSock;
-	hEventArr[numofClntSock] = newEvent;
-	++numofClntSock;
-
-	while (1)
-	{
-		posInfo = WSAWaitForMultipleEvents(numofClntSock, hEventArr, FALSE, WSA_INFINITE, FALSE);	//阻塞
-		startIdx = posInfo - WSA_WAIT_EVENT_0;
-
-		for (i = startIdx; i < numofClntSock; ++i)
-		{
-			int sigEventIdx = WSAWaitForMultipleEvents(1, &hEventArr[i], TRUE, 0, FALSE);	//立即返回
-			if (sigEventIdx == WSA_WAIT_FAILED || sigEventIdx == WSA_WAIT_TIMEOUT)
-				continue;
-			else
-			{
-				sigEventIdx = i;
-				WSAEnumNetworkEvents(hSockArr[sigEventIdx], hEventArr[sigEventIdx], &netEvents);
-				if (netEvents.lNetworkEvents & FD_ACCEPT)
-				{
-					if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
-					{
-						puts("accept error");
-						break;
-					}
-					clntAdrLen = sizeof(clntAdr);
-					hClntSock = accept(hSockArr[sigEventIdx], (SOCKADDR*)&clntAdr, &clntAdrLen);
-					newEvent = WSACreateEvent();
-					if (WSAEventSelect(hClntSock, newEvent, FD_READ | FD_WRITE | FD_CLOSE) == SOCKET_ERROR)
-						ErrorHandling("WSAEventSelect erorr");
-					hSockArr[numofClntSock] = hClntSock;
-					hEventArr[numofClntSock] = newEvent;
-					++numofClntSock;
-					printf("connected new cliend fd:%d\n", hClntSock);
-				}
-				if (netEvents.lNetworkEvents & FD_READ)
-				{
-					if (netEvents.iErrorCode[FD_READ_BIT] != 0)
-					{
-						puts("read error");
-						break;
-					}
-					memset(msg, 0, BUF_SIZE);
-					strLen = recv(hSockArr[sigEventIdx], msg, BUF_SIZE - 1, 0);
-					printf("recv fd:%d, msg:%s\n", hSockArr[sigEventIdx], msg);
-					send(hSockArr[sigEventIdx], msg, strLen, 0);
-				}
-				if (netEvents.lNetworkEvents & FD_CLOSE)
-				{
-					if (netEvents.iErrorCode[FD_CLOSE_BIT] != 0)
-					{
-						puts("close error");
-						break;
-					}
-					WSACloseEvent(hEventArr[sigEventIdx]);
-					closesocket(hSockArr[sigEventIdx]);
-					--numofClntSock;
-					CompressSockets(hSockArr, sigEventIdx, numofClntSock);
-					CompressEvents(hEventArr, sigEventIdx, numofClntSock);
-				}
-			}
-		}
-	}
-	WSACleanup();
-	system("pause");
-	return 0;
-}
-
-void CompressSockets(SOCKET hSockArr[], int idx, int total)
-{
-	int i;
-	for (i = idx; i < total; ++i)
-		hSockArr[i] = hSockArr[i + 1];
-}
-
-void CompressEvents(WSAEVENT hEventArr[], int idx, int total)
-{
-	int i;
-	for (i = idx; i < total; ++i)
-		hEventArr[i] = hEventArr[i + 1];
-}
-
-void ErrorHandling(char * msg)
-{
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
-
+﻿//#include <WinSock2.h>
+//#include <stdio.h>
+//#include <string.h>
+////#include <winsock2.h>
+//
+//#define BUF_SIZE 100
+//
+//void CompressSockets(SOCKET hSockArr[], int idx, int total);
+//void CompressEvents(WSAEVENT hEventArr[], int idx, int total);
+//void ErrorHandling(char* msg);
+//
+//int main(int argc, char* argv[])
+//{
+//	WSADATA wsaData = { 0 };
+//	SOCKET hServSock, hClntSock;
+//	SOCKADDR_IN servAdr, clntAdr;
+//
+//	SOCKET hSockArr[WSA_MAXIMUM_WAIT_EVENTS] = { 0 };
+//	WSAEVENT hEventArr[WSA_MAXIMUM_WAIT_EVENTS] = { 0 };
+//	WSAEVENT newEvent;
+//	WSANETWORKEVENTS netEvents;
+//
+//	int numofClntSock = 0;
+//	int strLen, i;
+//	int posInfo, startIdx;
+//	int clntAdrLen;
+//	char msg[BUF_SIZE] = { 0 };
+//
+//	if (argc != 2) {
+//		printf("Usage: %s <port>\n", argv[0]);
+//		exit(1);
+//	}
+//	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		ErrorHandling("WSAStartup error");
+//
+//	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+//	memset(&servAdr, 0, sizeof(servAdr));
+//	servAdr.sin_family = AF_INET;
+//	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+//	servAdr.sin_port = htons(atoi(argv[1]));
+//
+//	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+//		ErrorHandling("bind error");
+//
+//	if (listen(hServSock, 5) == SOCKET_ERROR)
+//		ErrorHandling("listen error");
+//
+//	newEvent = WSACreateEvent();
+//	if (WSAEventSelect(hServSock, newEvent, FD_ACCEPT) == SOCKET_ERROR)
+//		ErrorHandling("WSAEventSelect erorr");
+//	
+//	hSockArr[numofClntSock] = hServSock;
+//	hEventArr[numofClntSock] = newEvent;
+//	++numofClntSock;
+//
+//	while (1)
+//	{
+//		posInfo = WSAWaitForMultipleEvents(numofClntSock, hEventArr, FALSE, WSA_INFINITE, FALSE);	//阻塞
+//		startIdx = posInfo - WSA_WAIT_EVENT_0;
+//
+//		for (i = startIdx; i < numofClntSock; ++i)
+//		{
+//			int sigEventIdx = WSAWaitForMultipleEvents(1, &hEventArr[i], TRUE, 0, FALSE);	//立即返回
+//			if (sigEventIdx == WSA_WAIT_FAILED || sigEventIdx == WSA_WAIT_TIMEOUT)
+//				continue;
+//			else
+//			{
+//				sigEventIdx = i;
+//				WSAEnumNetworkEvents(hSockArr[sigEventIdx], hEventArr[sigEventIdx], &netEvents);
+//				if (netEvents.lNetworkEvents & FD_ACCEPT)
+//				{
+//					if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
+//					{
+//						puts("accept error");
+//						break;
+//					}
+//					clntAdrLen = sizeof(clntAdr);
+//					hClntSock = accept(hSockArr[sigEventIdx], (SOCKADDR*)&clntAdr, &clntAdrLen);
+//					newEvent = WSACreateEvent();
+//					if (WSAEventSelect(hClntSock, newEvent, FD_READ | FD_WRITE | FD_CLOSE) == SOCKET_ERROR)
+//						ErrorHandling("WSAEventSelect erorr");
+//					hSockArr[numofClntSock] = hClntSock;
+//					hEventArr[numofClntSock] = newEvent;
+//					++numofClntSock;
+//					printf("connected new cliend fd:%d\n", hClntSock);
+//				}
+//				if (netEvents.lNetworkEvents & FD_READ)
+//				{
+//					if (netEvents.iErrorCode[FD_READ_BIT] != 0)
+//					{
+//						puts("read error");
+//						break;
+//					}
+//					memset(msg, 0, BUF_SIZE);
+//					strLen = recv(hSockArr[sigEventIdx], msg, BUF_SIZE - 1, 0);
+//					printf("recv fd:%d, msg:%s\n", hSockArr[sigEventIdx], msg);
+//					send(hSockArr[sigEventIdx], msg, strLen, 0);
+//				}
+//				if (netEvents.lNetworkEvents & FD_CLOSE)
+//				{
+//					if (netEvents.iErrorCode[FD_CLOSE_BIT] != 0)
+//					{
+//						puts("close error");
+//						break;
+//					}
+//					WSACloseEvent(hEventArr[sigEventIdx]);
+//					closesocket(hSockArr[sigEventIdx]);
+//					--numofClntSock;
+//					CompressSockets(hSockArr, sigEventIdx, numofClntSock);
+//					CompressEvents(hEventArr, sigEventIdx, numofClntSock);
+//				}
+//			}
+//		}
+//	}
+//	WSACleanup();
+//	system("pause");
+//	return 0;
+//}
+//
+//void CompressSockets(SOCKET hSockArr[], int idx, int total)
+//{
+//	int i;
+//	for (i = idx; i < total; ++i)
+//		hSockArr[i] = hSockArr[i + 1];
+//}
+//
+//void CompressEvents(WSAEVENT hEventArr[], int idx, int total)
+//{
+//	int i;
+//	for (i = idx; i < total; ++i)
+//		hEventArr[i] = hEventArr[i + 1];
+//}
+//
+//void ErrorHandling(char * msg)
+//{
+//	fputs(msg, stderr);
+//	fputc('\n', stderr);
+//	exit(1);
+//}
+//
---
+++ Windows/IOCPEchoServ.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <process.h>
+#include <WinSock2.h>
+#include <Windows.h>
+
+#define BUF_SIZE 100
+#define READ 3
+#define WRITE 5
+
+typedef struct
+{
+	SOCKET hClntSock;
+	SOCKADDR_IN clntAddr;
+} PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
+
+typedef struct
+{
+	OVERLAPPED overlapped;
+	WSABUF wsaBuf;
+	char buffer[BUF_SIZE];
+	int rwMode;
+} PER_IO_DATA, *LPPER_IO_DATA;
+
+unsigned WINAPI EchoThreadMain(void* CompletionPort);
+void ErrorHandling(char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData = { 0 };
+	HANDLE comPort;
+	SYSTEM_INFO sysInfo;
+	LPPER_IO_DATA ioInfo;
+	LPPER_HANDLE_DATA handleInfo;
+	SOCKET servSock;
+	SOCKADDR_IN servAddr;
+	DWORD recvBytes;
+	DWORD flags = 0;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup error");
+
+	//根据CPU核数创建IO线程
+	comPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	GetSystemInfo(&sysInfo);
+	for (int i = 0; i < sysInfo.dwNumberOfProcessors; ++i)
+	{
+		_beginthreadex(NULL, 0, EchoThreadMain, comPort, 0, NULL);
+	}
+	//具有重叠功能的套接字
+	servSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = PF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(servSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("bind error");
+
+	if (listen(servSock, 5) == SOCKET_ERROR)
+		ErrorHandling("listen error");
+
+	while (1)
+	{
+		SOCKET clntSock;
+		SOCKADDR_IN clntAddr;
+		int addrLen = sizeof(clntAddr);
+
+		//servSock是阻塞的
+		clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &addrLen);
+		handleInfo = (LPPER_HANDLE_DATA)malloc(sizeof(PER_HANDLE_DATA));
+		handleInfo->hClntSock = clntSock;
+		memcpy(&handleInfo->clntAddr, &clntAddr, addrLen);
+
+		//将客户套接字与完成端口关联
+		CreateIoCompletionPort((HANDLE)clntSock, comPort, (DWORD)handleInfo, 0);
+
+		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+		memset(ioInfo, 0, sizeof(PER_IO_DATA));
+		ioInfo->wsaBuf.buf = ioInfo->buffer;
+		ioInfo->wsaBuf.len = BUF_SIZE;
+		ioInfo->rwMode = READ;
+		//ioInfo->overlapped->hEvent为空
+		WSARecv(handleInfo->hClntSock, &ioInfo->wsaBuf, 1, &recvBytes, &flags, &ioInfo->overlapped, NULL);
+	}
+	system("pause");
+	return 0;
+}
+
+unsigned WINAPI EchoThreadMain(void * pComPort)
+{
+	HANDLE comPort = (HANDLE)pComPort;
+	SOCKET sock;
+	DWORD bytesTrans;
+	LPPER_HANDLE_DATA handleInfo;
+	LPPER_IO_DATA ioInfo;
+	DWORD flags = 0;
+
+	while (1)
+	{
+		//从完成端口取出一个IO完成包
+		GetQueuedCompletionStatus(comPort, &bytesTrans, (LPDWORD)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
+		sock = handleInfo->hClntSock;
+		if (ioInfo->rwMode == READ)
+		{
+			printf("msg receive from client %d\n", sock);
+			if (bytesTrans == 0)
+			{
+				closesocket(sock);
+				free(handleInfo);
+				free(ioInfo);
+				continue;
+			}
+
+			memset(&ioInfo->overlapped, 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.len = bytesTrans;
+			ioInfo->rwMode = WRITE;
+			WSASend(sock, &ioInfo->wsaBuf, 1, NULL, 0, &ioInfo->overlapped, NULL);
+
+			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+			memset(&ioInfo, 0, sizeof(PER_IO_DATA));
+			ioInfo->wsaBuf.len = BUF_SIZE;
+			ioInfo->wsaBuf.buf = ioInfo->buffer;
+			ioInfo->rwMode = READ;
+			WSARecv(sock, &ioInfo->wsaBuf, 1, NULL, &flags, &ioInfo->overlapped, NULL);
+		}
+		else
+		{
+			printf("msg sended to client %d\n", sock);
+			free(ioInfo);
+		}
+	}
+	return 0;
+}
